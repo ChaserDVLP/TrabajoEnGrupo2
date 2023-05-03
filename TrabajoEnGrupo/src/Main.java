a7540d4de16214a886c4cdaafc3919d41a86b4be
raducristianj <raducristianj@a41e14-19.infor.com>
2023-05-03 20:49:42 +0200

    Hola

--- TrabajoEnGrupo/src/Main.java
+++ TrabajoEnGrupo/src/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.print("Mensaje escrito por Javi");
 		System.out.print("Mensaje escrito por Radu");
+		System.out.print("Mensaje escrito por Radu otra vez");
 	}
 }
